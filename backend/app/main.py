0a7f9a12be1e145193e07bb50aae349af3c0529f
p4rth <parthsingh1006@gmail.com>
2026-02-07 20:19:24 +0530

    add env variable on render

--- backend/app/main.py
+++ backend/app/main.py
@@ -18,7 +18,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[os.getenv("http://localhost:5173","https://green-trade-assignment.vercel.app")],
+    allow_origins=[os.getenv("FRONTEND_URL", "http://localhost:5173")],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
